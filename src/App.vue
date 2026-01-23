8e2d8f5592197e10c95392d6b25d3e3a718b72dc
Kokou DENYO <80518134+EmD-228@users.noreply.github.com>
2026-01-23 16:11:25 +0000

    refactor: update App structure and data
    - Update App.vue to use new section components
    - Update portfolio.json data
    - Update global styles

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <ErrorBoundary>
-    <div class="bg-primary-darker min-h-screen max-w-6xl mx-auto border-l border-r border-white">
-      <Header />
+    <div class="bg-primary-background min-h-screen max-w-full sm:max-w-6xl mx-auto lg:border-l lg:border-r lg:border-white">
+      <HeaderSection />
       <main>
         <div id="home">
           <HeroSection />
         </div>
         <AboutSection />
         <div id="works">
-          <StrategicOutcomes />
+          <ProjectsSection />
         </div>
         <ProcessSection />
         <TestimonialsSection />
         <div id="contact"></div>
       </main>
-      <Footer />
+      <FooterSection/>
     </div>
   </ErrorBoundary>
 </template>
@@ -23,11 +23,11 @@
 import { onErrorCaptured } from 'vue'
 import AboutSection from './components/AboutSection.vue'
 import ErrorBoundary from './components/ErrorBoundary.vue'
-import Footer from './components/Footer.vue'
-import Header from './components/Header.vue'
+import FooterSection from './components/FooterSection.vue'
+import HeaderSection from './components/HeaderSection.vue'
 import HeroSection from './components/HeroSection.vue'
 import ProcessSection from './components/ProcessSection.vue'
-import StrategicOutcomes from './components/StrategicOutcomes.vue'
+import ProjectsSection from './components/ProjectsSection.vue'
 import TestimonialsSection from './components/TestimonialsSection.vue'
 
 // Capturer les erreurs globales
